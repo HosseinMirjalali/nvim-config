aba244122490ba32e753252218213ffb9838f7ad
Panos Sakkos <panos.sakkos@panther.com>
2023-10-08 03:09:40 +0300

    remove redundant command

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,12 +3,3 @@ local opt = vim.opt
 opt.backup = false
 opt.swapfile = false
 
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end,
-})
-
