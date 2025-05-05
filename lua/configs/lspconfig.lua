2f37b308784556448ae18f3da033da82fdf769a4
Hossein <mirjalali.hosein@gmail.com>
2025-05-05 21:29:38 +0330

    Added some stuff

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -22,3 +22,21 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  on_init = nvlsp.on_init,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
